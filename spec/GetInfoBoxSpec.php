c922b6265fa6f85ce21765a4f9f6c78336a57bf4
Christopher Pecoraro <chrispecoraro@gmail.com>
2014-11-06 01:37:36 +0000

    switch to develop

---
+++ spec/GetInfoBoxSpec.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chris
+ * Date: 11/6/14
+ * Time: 2:30 AM
+ */
+
+namespace spec;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class GetInfoBoxSpec extends ObjectBehavior
+{
+}
